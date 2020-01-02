f0544bb1ee83ad674c80fb396afaffa00b30d98e
Liehko <juliette.laplante.jl@gmail.com>
2020-01-02 16:18:18 -0500

    added the sticku buttun to add field to a category

--- src/pages/BranchEdit.vue
+++ src/pages/BranchEdit.vue
@@ -66,6 +66,10 @@
             left-label/>
         </div>
       </div>
+
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn fab icon="add" color="primary" />
+      </q-page-sticky>
   </q-page>
 </template>
 
